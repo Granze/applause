237f90b27be43747125adf3c94de6ddadbe59c3e
Maurizio Mangione <granzebru@gmail.com>
2014-05-22 22:31:36 +0200

    markdown bug fixed

--- app/scripts/directives/markdown.js
+++ app/scripts/directives/markdown.js
@@ -2,9 +2,15 @@
 
 applause.directive('markdown', function ($window) {
 
+  if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+      return this.replace(/^\s+|\s+$/g, '');
+    };
+  }
+
   var converter = new $window.Showdown.converter(),
       linkFn = function (scope, element) {
-        element.html(converter.makeHtml(element.text()));
+        element.html(converter.makeHtml(element.text().trim()));
       };
 
   return {
