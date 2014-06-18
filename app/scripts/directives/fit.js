ca38ea607e72d3cdad9be4490f0689ad1d8f8a12
Maurizio Mangione <granzebru@gmail.com>
2014-06-18 20:33:48 +0200

    slide adapting on window resize

--- app/scripts/directives/fit.js
+++ app/scripts/directives/fit.js
@@ -2,24 +2,36 @@
 
 applause.directive('fit', function ($window) {
 
-  var slide = $window.document.querySelector('.slide'),
-      x = $window.innerWidth / slide.clientWidth,
-      y = $window.innerHeight / slide.clientHeight,
-      scale = Math.min(x, y),
-      fromTop = ($window.innerHeight - (slide.clientHeight * scale)) / 2,
-      fromLeft = ($window.innerWidth - (slide.clientWidth * scale)) / 2,
-      rule;
+  var linkFn = function (scope, element) {
+    scope.resizeSlide = function () {
+      var slide = $window.document.querySelector('.slide'),
+        x = $window.innerWidth / slide.clientWidth,
+        y = $window.innerHeight / slide.clientHeight,
+        scale = Math.min(x, y),
+        fromTop = ($window.innerHeight - (slide.clientHeight * scale)) / 2,
+        fromLeft = ($window.innerWidth - (slide.clientWidth * scale)) / 2,
+        rule = 'scale(' + scale + ')';
 
-  return function (scope, element) {
-    rule = 'scale(' + scale + ')';
-    element.css({
-      'transformOrigin': '0 0',
-      'webkitTransformOrigin': '0 0',
-      'transform': rule,
-      'webkitTransform': rule,
-      'top': fromTop + 'px',
-      'left': fromLeft + 'px'
+      element.css({
+        'transformOrigin': '0 0',
+        'webkitTransformOrigin': '0 0',
+        'transform': rule,
+        'webkitTransform': rule,
+        'top': fromTop + 'px',
+        'left': fromLeft + 'px'
+      });
+    };
+
+    scope.resizeSlide();
+
+    angular.element($window).bind('resize', function() {
+      scope.resizeSlide();
+      scope.$apply();
     });
+  };
+
+  return {
+    link: linkFn
   };
 
 });
