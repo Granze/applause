c7666eda3dbcf33bb52191c1e37a15f90e012819
Maurizio Mangione <granzebru@gmail.com>
2014-08-31 15:41:31 +0200

    fix undefined currentSlide

--- app/scripts/directives/progressbar.js
+++ app/scripts/directives/progressbar.js
@@ -1,10 +1,10 @@
 'use strict';
 
-applause.directive('progressBar', function () {
+applause.directive('progressBar', function() {
 
-  var linkFn = function (scope, element) {
-    scope.$watch(function (data) {
-      var p = (data.currentSlide / data.lastSlide * 100).toFixed(2);
+  var linkFn = function(scope, element) {
+    scope.$watch(function(data) {
+      var p = (scope.$storage.currentSlide / data.lastSlide * 100).toFixed(2);
       element.css('width', p + '%');
     });
   };
