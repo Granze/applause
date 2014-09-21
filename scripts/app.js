79f914c8423471ad4d6555b0c84ec5c0f149cdfb
Maurizio Mangione <granzebru@gmail.com>
2014-09-21 17:37:39 +0200

    higlightjs removed

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,4 @@
 /* jshint -W079 */
 'use strict';
 
-var applause = angular.module('applauseApp', ['ngAnimate', 'ngSanitize', 'hljs', 'ngStorage', 'applauseTemplates', 'applauseConfig']);
+var applause = angular.module('applauseApp', ['ngAnimate', 'ngSanitize', 'ngStorage', 'applauseTemplates', 'applauseConfig']);
