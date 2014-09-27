51a58935ae4a8f8d94dd643a46212254fc92bbec
Maurizio Mangione <granzebru@gmail.com>
2014-09-27 23:05:44 +0200

    Moved temporarily

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-{
-  "config": {
+angular.module('applauseConfig', [])
+  .constant('config', {
     "progressBar": true,
     "slideCount": true,
     "counter": true,
     "startFrom": 20,
-    "theme": "default"
-  }
-}
-
+    "theme": 'default'
+  });
