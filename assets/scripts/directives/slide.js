69de309f15b057f2b09c2d7149e991103f9da93b
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-05-18 20:47:06 +0200

    Cleaned browser navigation

--- assets/scripts/directives/slide.js
+++ assets/scripts/directives/slide.js
@@ -7,14 +7,25 @@ applause.directive('slide', function($document, $localStorage,$rootScope, Appdat
       linkFn = function(scope, element, attr) {
         scope.n = count;
         scope.isPreviewMode = Appdata.isPreviewMode;
+        scope.currentSlide = $localStorage.currentSlide;
+
+        scope.$watch(function(){
+          return $localStorage.currentSlide;
+        }, function(current, previous){
+          scope.currentSlide = current;
+        });
+
         Appdata.slides.push(count);
         count += 1;
 
+        // console.log(attr);
+
+        // Triggering global event for navigation
         if(scope.n === $localStorage.currentSlide){
           $document.bind('keydown', function(keyEvent){
             switch(keyEvent.keyCode) {
               case 27:
-                $rootScope.$emit('slide.goto', keyEvent);
+                $rootScope.$emit('slide.showGoTo', keyEvent);
                 break;
               case 37:
                 $rootScope.$emit('slide.prev', keyEvent);
@@ -28,7 +39,7 @@ applause.directive('slide', function($document, $localStorage,$rootScope, Appdat
       };
 
   return {
-    template: '<section class="slide" ng-class="{previous: n === $storage.currentSlide-1, current: n === $storage.currentSlide, next: n === $storage.currentSlide+1, preview: isPreviewMode}" ng-transclude fit></section>',
+    template: '<section class="slide" ng-class="{previous: n === currentSlide-1, current: n === currentSlide, next: n === currentSlide+1, preview: isPreviewMode}" ng-transclude fit></section>',
     restrict: 'E',
     transclude: true,
     replace: true,
