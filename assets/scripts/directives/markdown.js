32aa9f3c2d3bb1ff1bf6a6f4029ce091c2fad0b7
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-05-25 20:02:29 +0200

    Dirty hack to markdown

--- assets/scripts/directives/markdown.js
+++ assets/scripts/directives/markdown.js
@@ -4,13 +4,15 @@ applause.directive('markdown', function ($window) {
 
   if (!String.prototype.trim) {
     String.prototype.trim = function () {
-      return this.replace(/^\s+|\s+$/g, '');
+      return this.replace(/^\s+|\s+$/gmi, '');
     };
   }
 
   var converter = new $window.Showdown.converter(),
       linkFn = function (scope, element) {
-        element.html(converter.makeHtml(element.text().trim()));
+        var txt = element.text().trim();
+        console.log(txt);
+        element.html(converter.makeHtml(txt));
       };
 
   return {
